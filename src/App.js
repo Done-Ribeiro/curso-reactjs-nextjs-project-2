a85fbca7f0c218a4910d4ba0e00c7757549218da
Done Ribeiro <www.done_@hotmail.com>
2023-02-21 21:46:32 -0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,39 +1,60 @@
 import P from 'prop-types';
+import { useEffect, useState, useMemo } from 'react';
 import './App.css';
-import React, { useState, useEffect, useCallback } from 'react';
 
-// Precisa do React.memo para usar o Button (como um Cache)
-const Button = React.memo(function Button({ incrementButton }) {
-  console.log('Filho, renderizou');
-  return <button onClick={() => incrementButton(100)}>+</button>;
-});
+const Post = ({ post }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
 
-Button.propTypes = {
-  incrementButton: P.func,
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
 };
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
 
-  /*
-    aqui usamos o useCallback
-    para a partir da segunda chamada
-    ele renderizar somente o component pai
-  */
-  const incrementCounter = useCallback((num) => {
-    /*
-      para isso, não passaremos mais aqui o counter...
-      e sim o (prevCounter) como parâmetro da fn de callback
-    */
-    setCounter((c) => c + num);
+  console.log('Pai renderizou!');
+
+  // Component did mount
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((r) => r.json())
+        .then((r) => setPosts(r));
+    }, 5000);
   }, []);
-
-  console.log('Pai, renderizou');
 
   return (
     <div className="App">
-      <h1>C1: {counter}</h1>
-      <Button incrementButton={incrementCounter} />
+      <p>
+        <input
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} />;
+          })
+        );
+      }, [posts])}
+
+      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
     </div>
   );
 }
